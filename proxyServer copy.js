c8c7f132a96a9f01b545c3b02ca21c1de1281ab7
Gokula Krishnan <29031817+sgkrish007@users.noreply.github.com>
2024-07-24 17:22:39 +0530

    Add files via upload
    other files

---
+++ proxyServer copy.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/data_processing", async (req, res) => {
+  const url = `http://65.2.162.222:5000${req.url}`;
+  try {
+    console.log(`Forwarding request to: ${url}`);
+    const response = await axios({
+      method: req.method,
+      url: url,
+      data: req.body,
+      params: req.query,
+      headers: { ...req.headers, host: undefined },
+      timeout: 60000,
+    });
+    console.log("Response status:", response.status);
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error("Error from backend:", error);
+    if (error.code === "ECONNABORTED") {
+      res.status(504).send("Request timed out");
+    } else {
+      res
+        .status(error.response ? error.response.status : 500)
+        .send(error.message);
+    }
+  }
+});
+app.post('/data_processing', (req, res) => {
+  console.log("post", req);
+  res.send('POST,response.data');
+});
+  
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
+});
